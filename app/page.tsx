fbb958075efec3bb63e4f41e52d6e43ff685aff6
zihvvn <abozihaan@gmail.com>
2024-10-09 20:26:08 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='p-10 md:p-24 h-screen w-screen items-start flex flex-col justify-center gap-4'>
-      <h2 className='text-white text-xl font-normal'>abbdulla zihaan</h2>
+      <h2 className='text-white text-xl font-normal'>abdulla zihaan</h2>
 
       <p className='text-sm text-[#808083] font-light leading-7'>
         hello, i&apos;m a software engineer living in the maldives with a keen interest in researching/learning <i className='text-white'>backend development</i>. and also am quite active in{' '}<a href="https://www.x.com/zihvvn" className='text-white underline underline-offset-4'>twitter/x</a>
