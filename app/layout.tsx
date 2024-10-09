e07b8e76112dd0815d457b3238284928c3fa7d8e
Abdulla Zihaan <abozihaan@gmail.com>
2024-10-09 20:12:42 +0500

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Fira_Code } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const fira_code = Fira_Code ({ 
+  subsets: ['latin'], 
+  weight: ['300', '400', '500']
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <body className={fira_code.className}>
+      {children}
+    </body>
+  </html>
   );
 }
